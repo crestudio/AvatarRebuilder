788eb9327c8ea8374254b7fc14d6a06aaa3c7308
Macchiato <simppp@gmail.com>
2023-06-06 22:57:29 +0900

    아바타 핸들러 추가

---
+++ Assets/AvatarHandler.cs
@@ -0,0 +1,40 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+/*
+ * VRSuya AvatarRebuilder
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ * Forked from emilianavt/ReassignBoneWeigthsToNewMesh.cs ( https://gist.github.com/emilianavt/721cd4dd2d4a62ba54b002b63f894dbf )
+ * Thanks to Dalgona. & C_Carrot & Naru & Rekorn
+ */
+
+namespace com.vrsuya.avatarrebuilder {
+
+	[ExecuteInEditMode]
+	[AddComponentMenu("")]
+	public class AvatarHandler : AvatarRebuilder {
+
+		private enum AvatarStatus {
+			Patched, NeedPatch
+		}
+
+		/// <summary>
+		/// 본 프로그램의 메인 세팅 로직입니다.
+		/// </summary>
+		internal static void RequestCheckNewAvatar() {
+			switch (CheckNewAvatarStatus()) {
+				case AvatarStatus.NeedPatch:
+					break;
+				case AvatarStatus.Patched:
+					break;
+			}
+			return;
+		}
+
+		private static AvatarStatus CheckNewAvatarStatus() {
+			AvatarStatus Result = AvatarStatus.NeedPatch;
+			return Result;
+		}
+	}
+}
+#endif
