9ba741000ae0ff612ced794424403f736f843d44
Macchiato <simppp@gmail.com>
2024-07-05 21:16:01 +0900

    아바타 종류 추가

--- Assets/Runtime/AvatarRebuilder.cs
+++ Assets/Runtime/AvatarRebuilder.cs
@@ -39,7 +39,11 @@ namespace com.vrsuya.avatarrebuilder {
 		protected static Avatar TargetAvatar;
 
 		public enum Avatar {
-			NULL, Karin, MANUKA, Milk, Mint, Rusk, SELESTIA, Yoll
+			General, Aldina, Angura, Anon, Anri, Ash, Chiffon, Cygnet, Emmelie, EYO, Firina, Fuzzy, Glaze, Grus, Hakka, IMERIS,
+			Karin, Kikyo, Kokoa, Koyuki, Kuronatu, Lapwing, Leefa, Leeme, Lime, Lunalitt, Maki, Mamehinata, MANUKA, Mariel, Marron,
+			Maya, Merino, Milk, Minahoshi, Minase, Mint, Mir, Mishe, Moe, Nayu, Platinum, Quiche, Rainy,
+			Ramune_Old, RINDO, Rue, Rusk, SELESTIA, Sephira, Shinra, Sio, Sue, Sugar, Suzuhana, Tien, TubeRose, Ukon, Usasaki, Uzuki, Wolferia,
+			Yoll, YUGI_MIYO, Yuuko
 		}
 
 		protected enum BoneNameType {
